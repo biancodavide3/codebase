3a1cd482d5032cdfca466714f1f2e7f3d0a1357c
Davide Bianco <davidebianco1@icloud.com>
2025-03-20 12:29:43 +0100

    fix: parent pom and migration
    uncomment module in parent pom to correctly build project; remove comment from migration

--- user/src/main/resources/db/migration/V1.1__create-user-personal-information-table.sql
+++ user/src/main/resources/db/migration/V1.1__create-user-personal-information-table.sql
@@ -1,7 +1,3 @@
--- to add information related to a user create a new table like this one and make
--- relationship with the primary key in user_primary_key table
--- shared primary key model
-
 CREATE TABLE user_personal_information (
     id UUID PRIMARY KEY REFERENCES user_primary_key(id),
     name VARCHAR(255) NOT NULL,
