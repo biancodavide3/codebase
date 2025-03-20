af7d02596cd4539d0066850a91eb42fa652ce16f
Davide Bianco <davidebianco1@icloud.com>
2025-03-20 12:24:37 +0100

    create db entities
    for user_personal_information and user_primary_key tables matching the schema defined in flyway migrations and validated by spring data jpa

---
+++ user/src/main/java/com/biancodavide3/user/data/UserPrimaryKey.java
@@ -0,0 +1,33 @@
+package com.biancodavide3.user.data;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Entity(name = "UserPrimaryKey")
+@Table(name = "user_primary_key")
+@NoArgsConstructor
+public class UserPrimaryKey {
+    @Id
+    @Column(name = "id")
+    private final UUID id = UUID.randomUUID();
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        UserPrimaryKey that = (UserPrimaryKey) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
