e7619b954f6d2b05052b76041e9b43cb85642af8
Davide Bianco <davidebianco1@icloud.com>
2025-04-09 13:28:22 +0200

    create user security information request and response
    in clients module to share across microservices

---
+++ clients/src/main/java/com/biancodavide3/clients/user/UserSecurityInformationRequest.java
@@ -0,0 +1,4 @@
+package com.biancodavide3.clients.user;
+
+public record UserSecurityInformationRequest(String email, String password) {
+}
---
+++ clients/src/main/java/com/biancodavide3/clients/user/UserSecurityInformationResponse.java
@@ -0,0 +1,4 @@
+package com.biancodavide3.clients.user;
+
+public record UserSecurityInformationResponse(String email, String password) {
+}
