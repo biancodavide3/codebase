0a57fe8999ac2076754e31e30df9ac28c709dda0
Davide Bianco <davidebianco1@icloud.com>
2025-04-10 17:35:46 +0200

    refactor UserPrimaryKey
    make hibernate generate the UUID instead of java

--- user/src/main/java/com/biancodavide3/user/model/UserPrimaryKey.java
+++ user/src/main/java/com/biancodavide3/user/model/UserPrimaryKey.java
@@ -1,11 +1,9 @@
 package com.biancodavide3.user.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.Objects;
 import java.util.UUID;
@@ -14,10 +12,12 @@ import java.util.UUID;
 @Entity(name = "UserPrimaryKey")
 @Table(name = "user_primary_key")
 @NoArgsConstructor
+@ToString
 public class UserPrimaryKey {
     @Id
     @Column(name = "id")
-    private final UUID id = UUID.randomUUID();
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
     @Override
     public boolean equals(Object o) {
