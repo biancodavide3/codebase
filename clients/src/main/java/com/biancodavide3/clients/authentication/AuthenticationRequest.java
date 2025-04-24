ce6b8f4cb8bc3968c8231d1d06d5c001edc61b45
Davide Bianco <davidebianco1@icloud.com>
2025-04-24 09:40:00 +0200

    create authentication request and response
    they both store access and refresh token issued upon login or signup

---
+++ clients/src/main/java/com/biancodavide3/clients/authentication/AuthenticationRequest.java
@@ -0,0 +1,4 @@
+package com.biancodavide3.clients.authentication;
+
+public record AuthenticationRequest(String email, String password) {
+}
---
+++ clients/src/main/java/com/biancodavide3/clients/authentication/AuthenticationResponse.java
@@ -0,0 +1,4 @@
+package com.biancodavide3.clients.authentication;
+
+public record AuthenticationResponse (String accessToken, String refreshToken){
+}
