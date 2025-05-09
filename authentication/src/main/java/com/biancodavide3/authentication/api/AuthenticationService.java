3ae948657009d5daeb1d9f85ce00a08e86f867fb
Davide Bianco <davidebianco1@icloud.com>
2025-05-09 15:34:56 +0200

    correctly handle exceptions thrown by feign
    instead of using if statement

--- authentication/src/main/java/com/biancodavide3/authentication/api/AuthenticationService.java
+++ authentication/src/main/java/com/biancodavide3/authentication/api/AuthenticationService.java
@@ -4,12 +4,11 @@ import com.biancodavide3.clients.authentication.AuthenticationRequest;
 import com.biancodavide3.clients.authentication.AuthenticationResponse;
 import com.biancodavide3.clients.user.UserSecurityInformationClient;
 import com.biancodavide3.clients.user.UserSecurityInformationRequest;
-import com.biancodavide3.clients.user.UserSecurityInformationResponse;
 import com.biancodavide3.authentication.token.AccessTokenService;
 import com.biancodavide3.authentication.token.RefreshTokenService;
+import feign.FeignException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -27,9 +26,11 @@ public class AuthenticationService {
         String email = authenticationRequest.email();
         String password = passwordEncoder.encode(authenticationRequest.password());
         UserSecurityInformationRequest request = new UserSecurityInformationRequest(email, password);
-        ResponseEntity<UserSecurityInformationResponse> response = client.addUserSecurityInformation(request);
-        if (response.getStatusCode().isSameCodeAs(HttpStatus.CONFLICT))
+        try {
+            client.addUserSecurityInformation(request);
+        } catch (FeignException.Conflict e) {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();  // email taken
+        }
         String accessToken = accessTokenService.generateAccessToken(email);
         String refreshToken = refreshTokenService.generateRefreshToken(email);
         return ResponseEntity.ok(new AuthenticationResponse(accessToken, refreshToken));
@@ -39,12 +40,13 @@ public class AuthenticationService {
         String email = authenticationRequest.email();
         String password = passwordEncoder.encode(authenticationRequest.password());
         UserSecurityInformationRequest request = new UserSecurityInformationRequest(email, password);
-        ResponseEntity<UserSecurityInformationResponse> response = client.getUserSecurityInformation(request);
-        HttpStatusCode statusCode = response.getStatusCode();
-        if (statusCode.isSameCodeAs(HttpStatus.NOT_FOUND))
+        try {
+            client.getUserSecurityInformation(request);
+        } catch (FeignException.NotFound e) {
             return ResponseEntity.notFound().build();   // email not found
-        if (statusCode.isSameCodeAs(HttpStatus.CONFLICT))
-            return new ResponseEntity<>(HttpStatus.CONFLICT);   // wrong password
+        } catch (FeignException.Conflict e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT); // wrong password
+        }
         String accessToken = accessTokenService.generateAccessToken(email);
         String refreshToken = refreshTokenService.generateRefreshToken(email);
         return ResponseEntity.ok(new AuthenticationResponse(accessToken, refreshToken));
